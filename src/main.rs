194dd5bf902d96c2eb4280b73eddf34d48e6b2b6
Jon Olin <github.jao@jon.tomolin.net>
2020-04-18 00:03:47 -0400

    Add clap, some initial cli arg setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate clap;
+use clap::{Arg, App, crate_version};
+
 fn main() {
     println!("Hello, world!");
+
+    let matches = App::new("furry-parakeet")
+        .about("A shoddy VM for a poorly thought-out ISA.")
+        .version(crate_version!())
+        .arg(Arg::with_name("d1")
+             .help("File to mount as Disk 1.")
+             .long("d1")
+             .visible_alias("disk1")
+             .takes_value(true)
+             .value_name("FILE"))
+        .get_matches();
 }
